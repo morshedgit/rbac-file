f35a60db012734c82b919fad5d6ecc7d53878279
morshedgit <morshed.dev@gamil.com>
2023-11-15 22:22:40 +0000

    Adds RBAC with JDBC Support

---
+++ src/main/java/dev/samyar/TestUser.java
@@ -0,0 +1,61 @@
+package dev.samyar;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "test_user")
+public class TestUser {
+
+    @Id
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    // Constructors
+    public TestUser() {
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    // toString, hashCode, equals methods as needed
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,17 +1,9 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS test_user (
+  id INT,
+  username VARCHAR(255),
+  password VARCHAR(255),
+  role VARCHAR(255)
 );
 
-CREATE TABLE user_roles (
-    user_id INT,
-    role VARCHAR(255),
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
-);
-
-INSERT INTO users (username, password) VALUES ('admin2', 'admin2password');
-INSERT INTO users (username, password) VALUES ('user2', 'user2password');
-
-INSERT INTO user_roles (user_id, role) VALUES (1, 'ADMIN');
-INSERT INTO user_roles (user_id, role) VALUES (2, 'USER');
+INSERT INTO test_user (id, username, password, role) VALUES (1, 'admin', 'admin', 'admin');
+INSERT INTO test_user (id, username, password, role) VALUES (2, 'user','user', 'user');
